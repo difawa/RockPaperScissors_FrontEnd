16cd3b8b5caa381b3ba27f018c62481de2d39994
azkafargha <azkafarghaa@gmail.com>
2024-12-23 13:37:07 +0700

    integrasi backend regist dan login

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react';
-import { Text, View, Image, TouchableOpacity, Alert } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import InputBox from '../components/InputBox';
-import SubmitButton from '../components/SubmitButton';
-import { Link, useRouter } from 'expo-router';
+import { useState } from "react";
+import { Text, View, Image, TouchableOpacity, Alert } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import InputBox from "../components/InputBox";
+import SubmitButton from "../components/SubmitButton";
+import { Link, useRouter } from "expo-router";
+import { z } from "zod";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
+const LoginSchema = z.object({
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z.string().min(4, { message: "Must be 4 or more characters long" }),
+});
 
 export default function App() {
   const [appIsReady, setAppIsReady] = useState(false);
@@ -12,27 +18,70 @@ export default function App() {
   const [hidepassword, setHidePassword] = useState(true);
   const router = useRouter();
 
-  const handleSignIn = () => {
+  const handleSignIn = async () => {
     if (!form.email || !form.password) {
-      Alert.alert("Validation Error", "Please fill in all fields before signing in.");
+      Alert.alert(
+        "Validation Error",
+        "Please fill in all fields before signing in."
+      );
       return;
     }
+    try {
+      const res = await fetch("http://172.20.10.3:4000/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form),
+      });
 
-    console.log("Form Data:", form);
-    Alert.alert("Success", "Next, let's play the game!");
-    // Lakukan validasi tambahan atau pengiriman data ke backend di sini
-    router.replace("/main-menu");
+      const data = await res.json();
+      if (res.ok) {
+        const token = data.token;
+        console.log("Login berhasil:", token);
+
+        await AsyncStorage.setItem("userToken", token);
+        Alert.alert("Login berhasil dan token tersimpan!");
+
+        router.push("(home)"); //Ntar ganti ya maukemana
+      } else {
+        Alert.alert("Login Gagal", data.message || "Error terjadi.");
+      }
+    } catch (err) {
+      console.error("Error:", err);
+      Alert.alert("Error", "Terjadi kesalahan. Silakan coba lagi nanti.");
+    }
   };
 
   return (
-    <View
-      style={styles.container}>
-      <Image source={require('../assets/images/logo.png')} />
+    <View style={styles.container}>
+      <Image source={require("../assets/images/logo.png")} />
 
-      <InputBox text="Email" keyboardType="email-address" form={form} setForm={setForm} LowerCase={true} />
-      <InputBox text="Password" secureTextEntry={hidepassword} password={true} setPasswordVisible={setHidePassword} PasswordVisible={hidepassword} form={form} setForm={setForm} />
+      <InputBox
+        text="Email"
+        keyboardType="email-address"
+        form={form}
+        setForm={setForm}
+        LowerCase={true}
+      />
+      <InputBox
+        text="Password"
+        secureTextEntry={hidepassword}
+        password={true}
+        setPasswordVisible={setHidePassword}
+        PasswordVisible={hidepassword}
+        form={form}
+        setForm={setForm}
+      />
 
-      <Text style={{ color: '#fff', alignSelf: 'flex-start' }}>Don't have an account? <Link href={"/register"}><Text style={{ color: '#FEBB24', fontWeight: 'bold' }}>Register here</Text></Link></Text>
+      <Text style={{ color: "#fff", alignSelf: "flex-start" }}>
+        Don't have an account?{" "}
+        <Link href={"/register"}>
+          <Text style={{ color: "#FEBB24", fontWeight: "bold" }}>
+            Register here
+          </Text>
+        </Link>
+      </Text>
       <SubmitButton text="Sign In" onPress={handleSignIn} />
     </View>
   );
@@ -41,9 +90,9 @@ export default function App() {
 const styles = {
   container: {
     flex: 1,
-    backgroundColor: '#1A1C22',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20
-  }
+    backgroundColor: "#1A1C22",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
 };
--- app/register.jsx
+++ app/register.jsx
@@ -1,51 +1,116 @@
 import { Link, useRouter } from "expo-router";
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image, Alert } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  Alert,
+} from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import InputBox from "../components/InputBox";
 import SubmitButton from "../components/SubmitButton";
 import { useState } from "react";
 
 export default function Register() {
-    const [hidepassword, setHidePassword] = useState(true);
-    const [form, setForm] = useState({ username: "", email: "", password: "" });
-    const router = useRouter();
+  const [hidepassword, setHidePassword] = useState(true);
+  const [form, setForm] = useState({ username: "", email: "", password: "" });
+  const router = useRouter();
 
-    const handleSignUp = () => {
-        if (!form.username || !form.email || !form.password) {
-            Alert.alert("Validation Error", "Please fill in all fields before signing up.");
-            return;
-        } else if (/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(form.email) == false
-        ) {
-            Alert.alert("Validation Error", "Please enter a valid email address.");
-            return;
-        }
-        
-        // Jika semua input terisi, lanjutkan proses
-        console.log("Form Data:", form); // Debugging
-        Alert.alert("Success", "Your account has been created! Login now and play the game!");
-        // Lakukan validasi tambahan atau pengiriman data ke backend di sini
-        router.push("");
-    };
+  const handleSignUp = async () => {
+    if (!form.username || !form.email || !form.password) {
+      Alert.alert(
+        "Validation Error",
+        "Please fill in all fields before signing up."
+      );
+      return;
+    } else if (
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(form.email) ==
+      false
+    ) {
+      Alert.alert("Validation Error", "Please enter a valid email address.");
+      return;
+    }
+    // Mengirim data ke backend
+    try {
+      const response = await fetch("http://172.20.10.3:4000/auth/register", {
+        //localhost azka
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form),
+      });
 
-    return (
-        <View style={styles.container}>
-            <Image source={require('../assets/images/logo.png')} />
+      const data = await response.json();
 
-            <InputBox text="Username" form={form} setForm={setForm} LowerCase={true} />
-            <InputBox text="Email" keyboardType="email-address" form={form} setForm={setForm} LowerCase={true} />
-            <InputBox text="Password" secureTextEntry={hidepassword} password={true} setPasswordVisible={setHidePassword} PasswordVisible={hidepassword} form={form} setForm={setForm} />
-            <Text style={{ color: '#fff', alignSelf: 'flex-start' }}>Already have an account? <Link href={"/"}><Text style={{ color: '#FEBB24', fontWeight: 'bold' }}>Login here</Text></Link></Text>
-            <SubmitButton text="Sign Up" onPress={handleSignUp} />
-        </View>
-    );
+      if (response.ok) {
+        Alert.alert(
+          "Success",
+          "Your account has been created! Login now and play the game!"
+        );
+        router.push("/login");
+      } else {
+        Alert.alert(
+          "Error",
+          data.message || "Something went wrong. Please try again."
+        );
+      }
+    } catch (error) {
+      console.error("Error registering user:", error);
+      Alert.alert(
+        "Error",
+        "Failed to connect to the server. Please try again."
+      );
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image source={require("../assets/images/logo.png")} />
+
+      <InputBox
+        text="Username"
+        form={form}
+        setForm={setForm}
+        LowerCase={true}
+      />
+      <InputBox
+        text="Email"
+        keyboardType="email-address"
+        form={form}
+        setForm={setForm}
+        LowerCase={true}
+      />
+      <InputBox
+        text="Password"
+        secureTextEntry={hidepassword}
+        password={true}
+        setPasswordVisible={setHidePassword}
+        PasswordVisible={hidepassword}
+        form={form}
+        setForm={setForm}
+      />
+      <Text style={{ color: "#fff", alignSelf: "flex-start" }}>
+        Already have an account?{" "}
+        <Link href={"/"}>
+          <Text style={{ color: "#FEBB24", fontWeight: "bold" }}>
+            Login here
+          </Text>
+        </Link>
+      </Text>
+      <SubmitButton text="Sign Up" onPress={handleSignUp} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#1A1C22',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 20
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#1A1C22",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+});
