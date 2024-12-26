9e42ac28f2eb7244acfe413d6734f43dc8194017
Difa Winanda Adliya <difwinda@gmail.com>
2024-12-26 21:54:15 +0700

    UI EndGamePopOut update

--- components/EndGamePopOut.jsx
+++ components/EndGamePopOut.jsx
@@ -1,14 +1,15 @@
 import { View, Text, Modal, TouchableOpacity, StyleSheet } from 'react-native'
 import { useRouter } from 'expo-router'
+
 export default function EndGamePopOut({ visible, setVisible, finalGame }) {
     const router = useRouter()
     const textResult = function (finalGame) {
         if (finalGame === 'user1') {
-            return 'Game is Over! You Win!'
+            return (<Text style={{color: '#fff'}}>You <Text style={{color: '#009D47'}}>Win!</Text></Text>)
         } else if (finalGame === 'user2') {
-            return 'Game is Over! You Lose!'
+            return (<Text style={{color: '#fff'}}>You <Text style={{color: '#F80000'}}>Lose!</Text></Text>)
         } else if (finalGame === 'draw') {
-            return 'Game is Over! It\'s a Draw!'
+            return (<Text style={{color: '#fff'}}>It's a Draw!</Text>)
         } else {
             return 'Something wrong. Please try again.'
         }
@@ -21,19 +22,19 @@ export default function EndGamePopOut({ visible, setVisible, finalGame }) {
             onRequestClose={() => setVisible(false)}
         >
             <View style={styles.overlay}>
-                <View style={styles.modal}>
+                <View style={[styles.modal, { borderColor: finalGame === 'user1' ? '#009D47' : finalGame === 'user2' ? '#F80000' : '#fff' }]}>
                     <Text style={styles.modalText}>{textResult(finalGame)}</Text>
                     <TouchableOpacity
-                        style={styles.closeButton}
+                        style={styles.playAgain}
                         onPress={() => router.replace('/versuscom')}
                     >
-                        <Text style={styles.closeButtonText}>Play again!</Text>
+                        <Text style={styles.closeButtonText}>PLAY AGAIN</Text>
                     </TouchableOpacity>
                     <TouchableOpacity
                         style={styles.toLeaderboardButton}
                         onPress={() => router.replace('/leaderboard')}
                     >
-                        <Text style={styles.closeButtonText}>Go to Leaderboard</Text>
+                        <Text style={styles.closeButtonText}>GO TO LEADERBOARD</Text>
                     </TouchableOpacity>
                 </View>
             </View>
@@ -51,30 +52,32 @@ const styles = StyleSheet.create({
     modal: {
         width: 300,
         padding: 20,
-        backgroundColor: 'white',
+        backgroundColor: '#252728',
         borderRadius: 10,
         alignItems: 'center',
+        borderWidth: 5
     },
     modalText: {
         marginBottom: 15,
         textAlign: 'center',
-        fontSize: 18,
-        fontWeight: 'bold',
+        fontSize: 24,
+        fontFamily: 'BlackOpsOne',
     },
-    closeButton: {
+    playAgain: {
         padding: 10,
-        backgroundColor: '#000',
-        borderRadius: 5,
+        backgroundColor: '#D7E773',
+        borderRadius: 999,
     },
     closeButtonText: {
         color: '#fff',
-        fontWeight: 'bold',
+        fontFamily: 'BlackHanSans',
         textAlign: 'center',
+        textShadow: '0px 0px 5px black',
     },
     toLeaderboardButton: {
         marginTop: 10,
         padding: 10,
-        backgroundColor: '#FF5733',
-        borderRadius: 5,
+        backgroundColor: '#FEBB24',
+        borderRadius: 999,
     },
 })
